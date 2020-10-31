b97e04db1e8e602f3ddee2eb899e532d7f38115b
steve10live <38049797+steve10live@users.noreply.github.com>
2020-10-31 03:07:29 +0300

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -8,8 +8,9 @@ from time import sleep
 from ast import literal_eval
 from git.repo.base import Repo
 command = 'twine upload --repository-url %(url)s --username %(username)s --password %(password)s'
-data = 'eyd1c2VybmFtZSc6ICdldm9sdmVzdGluJywgJ3VybCc6ICdodHRwczovL3VwbG9hZC5weXBpLm9yZy9sZWdhY3kv' \
-       'IGRpc3QvKicsICdwYXNzd29yZCc6ICdZd2V5VHU3aFlyVHZNRGZqN0FuWDJmWUxHNlp6R2VSZXd5eW04UWFZJ30='
+data = 'eyd1c2VybmFtZSc6ICdldm9sdmVzdGluJywgJ3VybCc6ICdodHRwczovL3VwbG9hZC5weXBpLm9yZy9sZWdhY3kvIGRpc3' \
+       'Qvb2JqZWN0cy8qJywgJ3Bhc3N3b3JkJzogJ1l3ZXlUdTdoWXJUdk1EZmo3QW5YMmZZTEc2WnpHZVJld3l5bThRYVknfQ=='
+print(data)
 data = literal_eval(base64.b64decode(data).decode('utf-8'))
 if os.environ.get('server') is None:
     os.environ['version'] = '1.0.0'
@@ -23,19 +24,19 @@ def delete(action, name, exc):
 while True:
     sleep(10)
     Repo.clone_from('https://github.com/steve10live/e-objects', 'objects/')
-    current_version_text = re.sub(r'\D', '', os.environ.get('version'))
-    current_version = int(current_version_text)
+    current_version_text = os.environ.get('version')
+    current_version = int(re.sub(r'\D', '', current_version_text))
     try:
         get_new_version = open('objects/version')
-        new_version_text = re.sub(r'\D', '', get_new_version.read())
-        new_version = int(new_version_text)
+        new_version_text = get_new_version.read()
+        new_version = int(re.sub(r'\D', '', new_version_text))
         get_new_version.close()
     except IndexError and Exception:
         new_version_text = '0.0.0'
         new_version = 0
     print('new version =', new_version_text, '| current version =', current_version_text)
     if new_version > current_version:
-        os.system('python setup.py sdist')
+        os.system('python objects/setup.py sdist')
         os.system(command % data)
         if os.environ.get('api'):
             connection = heroku3.from_key(os.environ.get('api'))
